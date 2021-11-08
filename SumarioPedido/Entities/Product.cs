79f7b81079eb90ca3a07264b45c100ac997262d2
Bianca Longhi <longhisbianca@gmail.com>
2021-11-08 13:43:17 -0300

    Criando a classe Product e seus métodos

---
+++ SumarioPedido/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SumarioPedido.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; private set; }
+
+        public Product()
+        {
+
+        }
+       public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public void UpdatePrice(double newPrice)
+        {
+            Price = newPrice;
+        }
+    }
+}
